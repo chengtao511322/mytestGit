65ed78681fa0aa558d2197cb9cb7a4961682ccab
chengtao511322 <18088121818@163.com>
2020-10-24 22:29:33 +0800

    第二次添加了a1.java

---
+++ a1.java
@@ -0,0 +1,34 @@
+
+" zip.vim version v28
+" zip.vim version v28
+" zip.vim version v28
+" Browsing zipfile /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar
+" Select a file with cursor and press ENTER
+
+[/c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar]
+  End-of-central-directory signature not found.  Either this file is not
+  a zipfile, or it constitutes one disk of a multi-part archive.  In the
+  latter case the central directory and zipfile comment will be found on
+  the last disk(s) of this archive.
+zipinfo:  cannot find zipfile directory in one of /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar or
+          /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar.zip, and cannot find /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar.ZIP, period.
+" Browsing zipfile /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar
+" Select a file with cursor and press ENTER
+
+[/c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar]
+  End-of-central-directory signature not found.  Either this file is not
+  a zipfile, or it constitutes one disk of a multi-part archive.  In the
+  latter case the central directory and zipfile comment will be found on
+  the last disk(s) of this archive.
+zipinfo:  cannot find zipfile directory in one of /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar or
+          /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar.zip, and cannot find /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar.ZIP, period.
+" Browsing zipfile /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar
+" Select a file with cursor and press ENTER
+
+[/c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar]
+  End-of-central-directory signature not found.  Either this file is not
+  a zipfile, or it constitutes one disk of a multi-part archive.  In the
+  latter case the central directory and zipfile comment will be found on
+  the last disk(s) of this archive.
+zipinfo:  cannot find zipfile directory in one of /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar or
+          /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar.zip, and cannot find /c/Users/Administrator/Desktop/mygit/test/mytestGit/myjava123.jar.ZIP, period.
